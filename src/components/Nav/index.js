2138a9f3cb22da88e68bb15f63e62593e112c89f
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2023-04-25 07:00:32 -0400

    updated services

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,55 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import melkenLogo from '../../assets/images/Small.png';
 
+function serviceSelected(name) {
+    console.log(`${name} clicked`)
+  }
+
 function Navigation() {
+  const services = [
+    {
+      name: "Project Management",
+      description: "Achieve your project goals with ease and precision through Melken Solutions' proven project management approach."
+    },
+    {
+      name: "Interactive Electronic Technical Manuals (IETM)",
+      description: "Upgrade your technical game with IETMs - interactive, intuitive, and instant access to all your technical documentation needs."
+    },
+    {
+      name: "Engineering Support",
+      description: "Experience superior performance and cost savings with Melken Solutions' training system design and maintenance."
+    },
+    {
+      name: "Field Engineering Support",
+      description: "Solving complex problems on the ground with precision and expertise."
+    },
+    {
+      name: "Integrated Logistics Support (ILS)",
+      description: "From planning to implementation, Melken Solutions' integrated logistics support has you covered."
+    },
+    {
+      name: "Training Solutions",
+      description: "Maximize your training device ROI with Melken Solutions' tailored training programs."
+    },
+    {
+      name: "Data Entry",
+      description: "Maximize your productivity and streamline your operations with Melken Solutions' reliable data entry solutions."
+    },
+    {
+      name: "Information Management",
+      description: "Achieve optimal data organization and accessibility with Melken Solutions' expert information management services."
+    },
+    {
+      name: "508 Accessibility",
+      description: "Unlock your digital content's full potential with Melken Solutions' inclusive 508 Accessibility services."
+    },
+    {
+      name: "Document Conversion",
+      description: "Unlock the full potential of your documents with Melken Solutions' comprehensive conversion solutions."
+    }
+
+  ];
+
   return (
     <Navbar collapseOnSelect expand="lg" fixed="top" bg="light">
       <Container fluid="sm">
@@ -14,12 +62,25 @@ function Navigation() {
         <Navbar.Collapse className="justify-content-end">
           <Nav>
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#services">Our Services</Nav.Link>
-            <NavDropdown title="Government" id="navbarScrollingDropdown">
+            <NavDropdown title="Services">
+            {
+              services.map((service) => (
+                <NavDropdown.Item
+                  className="mx-1"
+                  key={service.name}
+                  >
+                    <span onClick={() => serviceSelected(service.name)} >
+                      {service.name}
+                    </span>
+                  </NavDropdown.Item>
+              ))}
+              </NavDropdown>
+
+            <NavDropdown title="Government">
               <NavDropdown.Item href="#capability-statement">Capability Statement</NavDropdown.Item>
               <NavDropdown.Item href="#contract-vehicles">Contract Vehicles</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="About" id="navbarScrollingDropdown">
+            <NavDropdown title="About">
               <NavDropdown.Item href="#news-events">History</NavDropdown.Item>
               <NavDropdown.Item href="#news-events">News & Events</NavDropdown.Item>
               <NavDropdown.Item href="#contact">Contact</NavDropdown.Item>
