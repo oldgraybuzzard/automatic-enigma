7e6545d5762cbdd57d2d6f6a08cab9e7929d0db9
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2023-04-25 16:25:23 -0400

    move the service list to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from './components/Nav';
 import About from './components/About';
 import ServiceGallery from './components/Services';
 
 
 function App() {
+  const [services] = useState([
+    {
+      name: "Project Management",
+      description: "Achieve your project goals with ease and precision through Melken Solutions' proven project management approach."
+    },
+    {
+      name: "Interactive Electronic Technical Manuals (IETM)",
+      description: "Upgrade your technical game with IETMs - interactive, intuitive, and instant access to all your technical documentation needs."
+    },
+    {
+      name: "Engineering Support",
+      description: "Experience superior performance and cost savings with Melken Solutions' training system design and maintenance."
+    },
+    {
+      name: "Field Engineering Support",
+      description: "Solving complex problems on the ground with precision and expertise."
+    },
+    {
+      name: "Integrated Logistics Support (ILS)",
+      description: "From planning to implementation, Melken Solutions' integrated logistics support has you covered."
+    },
+    {
+      name: "Training Solutions",
+      description: "Maximize your training device ROI with Melken Solutions' tailored training programs."
+    },
+    {
+      name: "Data Entry",
+      description: "Maximize your productivity and streamline your operations with Melken Solutions' reliable data entry solutions."
+    },
+    {
+      name: "Information Management",
+      description: "Achieve optimal data organization and accessibility with Melken Solutions' expert information management services."
+    },
+    {
+      name: "508 Accessibility",
+      description: "Unlock your digital content's full potential with Melken Solutions' inclusive 508 Accessibility services."
+    },
+    {
+      name: "Document Conversion",
+      description: "Unlock the full potential of your documents with Melken Solutions' comprehensive conversion solutions."
+    },
+  ]);
+
+  const [currentService, setCurrentService] = useState(services[0]);
+
   return (
     <div>
-        <Navigation></Navigation>
-        <main>
+      <Navigation
+        services={services}
+        setCurrentService={setCurrentService}
+        currentService={currentService}
+      ></Navigation>
+      <main>
+        <div>
           <ServiceGallery></ServiceGallery>
           <About></About>
-        </main> 
+        </div>
+      </main> 
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,55 +6,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import melkenLogo from '../../assets/images/Small.png';
 
-function serviceSelected(name) {
-    console.log(`${name} clicked`)
-  }
+function Navigation(props) {
+  const {
+    services = [],
+    setCurrentService,
+    currentService,
+  } = props;
 
-function Navigation() {
-   const [services] = useState([
-    {
-      name: "Project Management",
-      description: "Achieve your project goals with ease and precision through Melken Solutions' proven project management approach."
-    },
-    {
-      name: "Interactive Electronic Technical Manuals (IETM)",
-      description: "Upgrade your technical game with IETMs - interactive, intuitive, and instant access to all your technical documentation needs."
-    },
-    {
-      name: "Engineering Support",
-      description: "Experience superior performance and cost savings with Melken Solutions' training system design and maintenance."
-    },
-    {
-      name: "Field Engineering Support",
-      description: "Solving complex problems on the ground with precision and expertise."
-    },
-    {
-      name: "Integrated Logistics Support (ILS)",
-      description: "From planning to implementation, Melken Solutions' integrated logistics support has you covered."
-    },
-    {
-      name: "Training Solutions",
-      description: "Maximize your training device ROI with Melken Solutions' tailored training programs."
-    },
-    {
-      name: "Data Entry",
-      description: "Maximize your productivity and streamline your operations with Melken Solutions' reliable data entry solutions."
-    },
-    {
-      name: "Information Management",
-      description: "Achieve optimal data organization and accessibility with Melken Solutions' expert information management services."
-    },
-    {
-      name: "508 Accessibility",
-      description: "Unlock your digital content's full potential with Melken Solutions' inclusive 508 Accessibility services."
-    },
-    {
-      name: "Document Conversion",
-      description: "Unlock the full potential of your documents with Melken Solutions' comprehensive conversion solutions."
-    },
-  ]);
-
-  const [currentService, setCurrentService] = useState(services[0]);
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentService.name);
+  }, [currentService]);
 
   return (
     <Navbar collapseOnSelect expand="lg" sticky="top" bg="light">
