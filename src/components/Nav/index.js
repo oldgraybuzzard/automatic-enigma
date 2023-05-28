a1232567aaf5e182235e1b17f29dfc3171867ead
oldgraybuzzard <k_felder@me.com>
2023-05-27 21:30:49 -0400

    modiifed the navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
-import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import melkenLogo from '../../assets/images/Small.png';
-// import EmployeeArea from '../EmployeeArea';
-import* as msal from '@azure/msal-browser';
+import * as msal from '@azure/msal-browser';
 
 const msalConfig = {
   auth: {
     clientId: 'ea48be00-4342-4789-9162-641dd9be4009',
     authority: 'https://login.microsoftonline.com/281950d2-3875-419e-8017-cf5cf823f049',
-    redirectUri: window.location.origin
-  }
+    redirectUri: window.location.origin,
+  },
 };
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
@@ -25,35 +23,35 @@ function Navigation() {
   const [services] = useState([
     {
       name: 'Project Management',
-      id: '#project-management'
+      id: '#project-management',
     },
     {
       name: 'Interactive Electronic Technical Manuals (IETM)',
-      id: '#ietm'
+      id: '#ietm',
     },
     {
       name: 'Engineering Support',
-      id: '#engineering-support'
+      id: '#engineering-support',
     },
     {
       name: 'Integrated Logistics Support (ILS)',
-      id: '#ils'
+      id: '#ils',
     },
     {
       name: 'Training Solutions',
-      id: '#training-solutions'
+      id: '#training-solutions',
     },
     {
       name: '508 Accessibility',
-      id: '#508'
+      id: '#508',
     },
     {
       name: 'Information Management',
-      id: '#information-management'
+      id: '#information-management',
     },
     {
       name: 'Data Entry',
-      id: '#data-entry'
+      id: '#data-entry',
     },
   ]);
 
@@ -61,65 +59,67 @@ function Navigation() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = async () => {
-   try {
-      // Login with Microsoft 365 credentials
+    try {
       const loginRequest = {
-        scopes: ['user.read', 'openid', 'profile']
+        scopes: ['user.read', 'openid', 'profile'],
       };
 
       const response = await msalInstance.loginPopup(loginRequest);
       if (response && response.account) {
-        // Authentication successful
         setLoggedIn(true);
       }
     } catch (error) {
       console.log(error);
-      // Handle error
     }
-  }
+  };
 
   const handleEmployeePage = () => {
     window.open('https://melkensolutions.sharepoint.com/sites/MelkenIntranet', '_blank');
   };
-  
+
   return (
     <Navbar id="navbar" collapseOnSelect expand="lg" sticky="top">
-      <Navbar.Brand as={Link} to="/">
-          <img className="logo" src={melkenLogo} alt="Melken Brand Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <NavDropdown title="Services">
-              {services.map((service) => (
-                <NavDropdown.Item
-                  href={service.id}
-                  className={`mx-1 ${currentService.name === service.name ? 'navActive' : ''}`}
-                  key={service.name}
-                  onClick={() => setCurrentService(service)}
-                >
-                  {capitalizeFirstLetter(service.name)}
-                </NavDropdown.Item>
-              ))}
-            </NavDropdown>
-            <NavDropdown title="Government">
-              <NavDropdown.Item href="#capability">Capability Statement</NavDropdown.Item>
-              <NavDropdown.Item href="#contract-vehicles">Contract Vehicles</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="About">
-              <NavDropdown.Item href="#history">History</NavDropdown.Item>
-              <NavDropdown.Item href="#certifications">Certifications</NavDropdown.Item>
-              <NavDropdown.Item href="#contactUs">Contact</NavDropdown.Item>
-            </NavDropdown>
-             {loggedIn ? (
-                <Nav.Link onClick={handleEmployeePage}>Employee Page</Nav.Link>
-              ) : (
-                <Button variant="secondary" onClick={handleLogin}>Employee Login</Button>
-              )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Navbar.Brand as={Link} to="/" className="mx-5">
+        <img className="logo" src={melkenLogo} alt="Melken Brand Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link href="/" className="mx-5">Home</Nav.Link>
+          <NavDropdown title="Services" className="mx-5">
+            {services.map((service) => (
+              <NavDropdown.Item
+                href={service.id}
+                className={`mx-1 ${currentService.name === service.name ? 'navActive' : ''}`}
+                key={service.name}
+                onClick={() => setCurrentService(service)}
+                style={{ margin: '0.5rem 1rem' }}
+              >
+                {capitalizeFirstLetter(service.name)}
+              </NavDropdown.Item>
+            ))}
+          </NavDropdown>
+          <NavDropdown title="Government" className="mx-5">
+            <NavDropdown.Item href="#capability">Capability Statement</NavDropdown.Item>
+            <NavDropdown.Item href="#contract-vehicles">Contract Vehicles</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="About" className="mx-5">
+            <NavDropdown.Item href="#history">History</NavDropdown.Item>
+            <NavDropdown.Item href="#certifications">Certifications</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#contactUs" className="mx-5">Contact Us</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse>
+        <Nav>
+          {loggedIn ? (
+            <Nav.Link onClick={handleEmployeePage}>Employee Page</Nav.Link>
+          ) : (
+            <Button variant="secondary" onClick={handleLogin}>Employee Login</Button>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
