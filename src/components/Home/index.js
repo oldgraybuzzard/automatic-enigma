c4514da1e69ea3e62be3d3a3188244ea89c4f3d1
oldgraybuzzard <k_felder@me.com>
2023-06-28 21:14:54 -0400

    more moves to MDB

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Row from 'react-bootstrap/Row';
-import Carousel from 'react-bootstrap/Carousel';
+import {
+  MDBCarousel,
+  MDBCarouselItem,
+} from 'mdb-react-ui-kit';
 import { capitalizeFirstLetter } from "../../utils/helpers";
-import "../../carousel.css";
 import hero1 from '../../assets/images/hero1.jpg';
 import hero2 from '../../assets/images/hero2.jpg';
 import hero3 from '../../assets/images/hero3.jpg';
@@ -14,56 +16,44 @@ function Home() {
       <Row>
       <h1 className="text-center mbr-section-title mbr-fonts-style mbr-pt-1 mbr-bold display-4">{capitalizeFirstLetter('The Success of Your Mission Is Our Mission!')}</h1>
       </Row>
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
+    <MDBCarousel showControls showIndicators dealy={8000}>
+      <MDBCarouselItem
+          className="d-block w-100 carouselImage"
+          itemId={1}
           src={hero1}
           alt="Blackbaord with the word Problem and Solution"
-        />
-        <Carousel.Caption>
+      >
           {capitalizeFirstLetter}<h3>Identifying problems.</h3>
           {capitalizeFirstLetter}<p>Delivering Innovative Solutions.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      </MDBCarouselItem>
       
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
+      <MDBCarouselItem
+          className="d-block w-100 carouselImage"
+          itemId={2}
           src={hero2}
           alt="Military vessel at sea"
-        />
-
-        <Carousel.Caption>
+      >
           {capitalizeFirstLetter}<h3>Assisting Our Clients with State-of-the-Art Solutions.</h3>
           {capitalizeFirstLetter}<p>From High-Tech Simulators to Realistic Training Environments.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
+      </MDBCarouselItem>
+      <MDBCarouselItem
+          className="d-block w-100 carouselImage"
+          itemId={3}
           src={hero3}
           alt="HTML code"
-        />
-
-      <Carousel.Caption>
+      >
         <h3>{capitalizeFirstLetter('Unlocking Efficiency and Enhancing Accessibility with Interactive Electronic Technical Manuals (IETMs)')}</h3>
         <p>{capitalizeFirstLetter('Delivering Interactive and Intuitive Technical Documentation Solutions.')}</p>
-      </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
+      </MDBCarouselItem>
+      <MDBCarouselItem
+          className="d-block w-100 carouselImage"
+          itemId={4}
           src={hero4}
           alt="Chessboard with pieces"
-        />
-
-      <Carousel.Caption>
+      >
         <h3>{capitalizeFirstLetter('Streamline Your Projects with Expert Management.')}</h3>
-        <p>{capitalizeFirstLetter('Efficiently Delivering Success, Every Step of the Way.')}</p>
-      </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      </MDBCarouselItem>
+    </MDBCarousel>
   </section>      
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
-import { MDBBtn } from 'mdb-react-ui-kit';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBBtn,
+  MDBIcon
+} from 'mdb-react-ui-kit';
 import { Link } from 'react-router-dom';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import melkenLogo from '../../assets/images/Small.png';
+import melkenLogo from '../../assets/images/very-big-320x59.png';
 import * as msal from '@azure/msal-browser';
 import JsonServiceList from "../../assets/json/services.json";
 
@@ -20,10 +32,11 @@ const msalConfig = {
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 function Navigation() {
-  const serviceList=JsonServiceList;
-  
+  const serviceList = JsonServiceList;
   const [currentService, setCurrentService] = useState(serviceList[0]);
   const [loggedIn, setLoggedIn] = useState(false);
+
+  const [showNavCentred, setShowNavCentred] = useState(false);
 
   const handleLogin = async () => {
     try {
@@ -45,54 +58,106 @@ function Navigation() {
   };
 
   return (
-    <Navbar id="navbar" collapseOnSelect expand="xl" sticky="top">
-      <Navbar.Brand as={Link} to="/" className="mx-3">
-        <img className="logo" src={melkenLogo} alt="Melken Brand Logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse>
-        <Nav>
-          <Nav.Link href="/" className="mx-3">Home</Nav.Link>
-          <NavDropdown title="Services" className="mx-3">
-            {serviceList.map((service) => (
-              <NavDropdown.Item
-                href={service.link}
-                className={`mx-1 ${currentService.name === service.name ? 'navActive' : ''}`}
-                key={service.name}
-                onClick={() => setCurrentService(service)}
-                style={{ margin: '0.5rem 1rem' }}
-              >
-                {capitalizeFirstLetter(service.name)}
-              </NavDropdown.Item>
-            ))}
-          </NavDropdown>
-          <NavDropdown title="Government" className="mx-3">
-            <NavDropdown.Item href="#capability">Capability Statement</NavDropdown.Item>
-            <NavDropdown.Item href="#contract-vehicles">Contract Vehicles</NavDropdown.Item>
-          </NavDropdown>
-          <NavDropdown title="About" className="mx-3">
-            <NavDropdown.Item href="#history">History</NavDropdown.Item>
-            <NavDropdown.Item href="#certifications">Certifications</NavDropdown.Item>
-           
-          </NavDropdown>
-          <NavDropdown title="Resources" className='mx-3'>
-            <NavDropdown.Item href="#news">News</NavDropdown.Item>
-            <NavDropdown.Item href="#success">Success Cases</NavDropdown.Item>
-            <NavDropdown.Item href="https://melkensolutions.wordpress.com/" target="_blank">Blog</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="#contactUs" className="mx-3">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-      <Navbar.Collapse>
-        <Nav>
+    <MDBNavbar expand="lg" sticky light className='w-auto p-3 mb-2 bg-light bg-gradient text-dark rounded-5'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand as={Link} to="/">
+          <img
+            className="logo"
+            src={melkenLogo}
+            alt="Melken Brand Logo"
+            loading='lazy'
+          />
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+            data-target='#navbarCentered'
+            aria-controls='navbarCentered'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNavCentred(!showNavCentred)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showNavCentred} center id="navbarCentered">
+          <MDBNavbarNav breakpoint="lg" className='justify-content-end'>
+            <MDBNavbarItem className='active text-uppercase fw-bold'>
+              <MDBNavbarLink href="/">Home</MDBNavbarLink>
+            </MDBNavbarItem>
+
+            <MDBDropdown>
+              <MDBDropdownToggle tag="a" className="nav-link text-uppercase fw-bold" role='button'>
+                Services
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className='bg-info p-2 fs-6'>
+                {serviceList.map((service) => (
+                  <MDBDropdownItem
+                    href={service.link}
+                    className={`mx-1 ${currentService.name === service.name ? 'navActive' : ''}`}
+                    key={service.name}
+                    onClick={() => setCurrentService(service)}
+                  >
+                    {capitalizeFirstLetter(service.name)}
+                  </MDBDropdownItem>
+                ))}
+              </MDBDropdownMenu>
+            </MDBDropdown>
+
+            <MDBDropdown>
+              <MDBDropdownToggle tag="a" className="nav-link text-uppercase fw-bold" role='button'>
+                Government
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className='bg-info p-2 fs-6'>
+                <MDBDropdownItem href="#capability">Capability Statement</MDBDropdownItem>
+                <MDBDropdownItem href="#contract-vehicles">Contract Vehicles</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+
+            <MDBDropdown>
+              <MDBDropdownToggle tag="a" className="nav-link text-uppercase fw-bold" role='button'>
+                About
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className='bg-info p-2 fs-6'>
+                <MDBDropdownItem href="#history">History</MDBDropdownItem>
+                <MDBDropdownItem href="#certifications">Certifications</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+
+            <MDBDropdown>
+              <MDBDropdownToggle tag="a" className="nav-link text-uppercase fw-bold" role='button'>
+                Resources
+              </MDBDropdownToggle >
+              <MDBDropdownMenu className='bg-info p-2 fs-6'>
+                <MDBDropdownItem href="#news">News</MDBDropdownItem>
+                <MDBDropdownItem href="#success">Success Cases</MDBDropdownItem>
+                <MDBDropdownItem href="https://melkensolutions.wordpress.com/" target="_blank">
+                  Blog
+                </MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+
+            <MDBNavbarItem className='text-uppercase fw-bold'>
+              <MDBNavbarLink href="#contactUs">Contact</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        
+
+        <MDBNavbarNav className='justify-content-end'>
           {loggedIn ? (
-            <Nav.Link onClick={handleEmployeePage}>Melken Magnet</Nav.Link>
+            <MDBNavbarItem>
+              <MDBNavbarLink onClick={handleEmployeePage}>Melken Magnet</MDBNavbarLink>
+            </MDBNavbarItem>
           ) : (
-            <MDBBtn rounded color='link' onClick={handleLogin}>Employee Login</MDBBtn>
+            <MDBNavbarItem>
+              <MDBBtn rounded color='secondary' onClick={handleLogin}>
+                Employee Login
+              </MDBBtn>
+            </MDBNavbarItem>
           )}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+        </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
   );
 }
 
